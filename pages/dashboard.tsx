1b2a39934b25700c8a7e0bb7c2e8eca0e1601495
BrenoAugustoo <brenoaugusto98@gmail.com>
2022-02-12 23:41:20 -0300

    Autenticação no SSR do Next

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,13 @@
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can";
 import { AuthContext, signOut } from "../contexts/AuthContext"
-import { api } from "../services/api";
+import { useCan } from "../hooks/useCan";
+import { setupApiClient } from "../services/api";
+import { api } from "../services/apiClient";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard () {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get('/me').then(response => {
@@ -14,6 +18,25 @@ export default function Dashboard () {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}> Sign Out </button>
+
+      <Can permissions={['metrics.list']} >
+        <div>Métricas</div>
+      </Can>
+    </>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupApiClient(ctx);
+  const response = await apiClient.get('/me');
+  
+  console.log(response);
+
+  return {
+    props: {}
+  }
+})
